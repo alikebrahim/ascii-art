8304d5419907743e6b93c4b03e89736c4b77fc0c
aliebrahim <alikebrabim@me.com>
2023-07-04 19:11:50 +0300

    Prep for fs

--- main.go
+++ main.go
@@ -8,36 +8,42 @@ import (
 	"os"
 )
 
-
-
 func main() {
 
 	// TODO: CMD line args handling
 	args := os.Args
 	flag.Parse()
 
-	fmt.Println(flag.NArg(), flag.NFlag())
-	// fmt.Println("*Justify: ", asciiart.Justify, " *Color: ",asciiart.Color, "*Output: ", asciiart.Output)
+	fmt.Println(len(args))
+	if len(args) != 3 {
+		fmt.Println("Usage: go run . [STRING] [BANNER]")
+		fmt.Println("EX: go run . something standard")
+		return
+	}
+
+	// fmt.Println(flag.NArg(), flag.NFlag())
+	// fmt.Println("*Justify: ", asciiart.Justify, " *Color: ", asciiart.Color, "*Output: ", asciiart.Output)
 	// os.Exit(1)
 
-	if flag.NFlag() == 0 && len(args) != 3 {
-		fmt.Println("Entered @ 0")
-		fmt.Println("Usage: go run . [STRING] [BANNER]")
-		fmt.Println("EX: go run . something standard")
-		return
-	} else if flag.NFlag() != 0 && len(args) != 5 {
-		fmt.Println("Entered @ > 0")
-		fmt.Println("Usage: go run . [STRING] [BANNER]")
-		fmt.Println("EX: go run . something standard")
-		return
-	}
+	// if flag.NFlag() == 0 && len(args) != 3 {
+	// 	fmt.Println("Entered @ 0")
+	// 	fmt.Println("Usage: go run . [STRING] [BANNER]")
+	// 	fmt.Println("EX: go run . something standard")
+	// 	return
+	// } else if flag.NFlag() != 0 && len(args) != 5 {
+	// 	fmt.Println("Entered @ > 0")
+	// 	fmt.Println("Usage: go run . [STRING] [BANNER]")
+	// 	fmt.Println("EX: go run . something standard")
+	// 	return
+	// }
 
-	var bannerFile string
-	if flag.NFlag() == 0 {
-		bannerFile = args[2] + ".txt"
-	} else {
-		bannerFile = args[4] + ".txt"
-	}
+	bannerFile := args[2] + ".txt"
+	// var bannerFile string
+	// if flag.NFlag() == 0 {
+	// 	bannerFile = args[2] + ".txt"
+	// } else {
+	// 	bannerFile = args[4] + ".txt"
+	// }
 
 	// Testing banner
 	reader, err := fs.ReadFile(os.DirFS("./banners"), bannerFile)
